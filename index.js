274dbbd1d66d16af3af0d0848683078251126252
Azharrabbani <azharr1203@gmail.com>
2025-02-25 23:34:54 +0700

    started backend logic

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ dotenv.config();
 const app = express();
 const port = 4000;
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
 
 const db = new pg.Client({
     user: process.env.DB_USER,
@@ -18,7 +19,33 @@ const db = new pg.Client({
 });
 
 
+app.get('/books', async (req, res) => {
+    try{
+        const result = await db.query("SELECT * FROM book INNER JOIN book_type ON book_type.id = book.book_type");
+        res.json(result.rows);
+    } catch (error){
+        res.status(500).json({error: error.message});
+    }
+});
 
+app.get('/books-type', async (req, res) => {
+    try{
+        const result = await db.query("SELECT * FROM book_type");
+        res.json(result.rows);
+    } catch (error){
+        res.status(500).json({error : error.message});
+    }
+});
+
+app.post('/add-book-type', async (req, res) => {
+    const type = req.body.book_type;
+    try{
+        const result = await db.query("INSERT INTO book_type (type_title) VALUES ($1) RETURNING *", [type]);
+        res.status(201).json(result.rows[0]);
+    } catch (error){
+        res.status(500).json({error: error.message});
+    }
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 
@@ -10,18 +10,41 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
-})
+app.get('/', async (req, res) => {
+    try{
+        const response_book = await axios.get(`${API_URL}/book`);
+        const response_type = await axios.get(`${API_URL}/book-type`);
+        res.render('index.ejs', {
+            books: response_book.data,
+            types: response_type.data
+        });
+    } catch (error){
+        res.render('index.ejs', {
+            books: [], 
+            types: [],
+            error: error.message
+        });
+    }
+});
+
+app.post('/type', async (req, res) => {
+    console.log("Received Data:", req.body); 
+    try{
+        await axios.post(`${API_URL}/add-book-type`, req.body);
+        res.redirect('/');
+    } catch (error){
+        res.redirect('/');
+    }
+});
 
 app.get('/add', (req, res) => {
     res.render('addPage.ejs');
-})
+});
 
 app.get('/book', (req, res) => {
     res.render('detail.ejs');
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
-})
+});
